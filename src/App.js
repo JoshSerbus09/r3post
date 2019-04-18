4ec674b2168af4ce898c7d81b0430c79b47df341
Josh Serbus <JoshSerbus09@gmail.com>
2019-04-17 19:48:18 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          R3POST
+        </header>
+        
+        <body>
+          <Main />
+        </body>
+        
+        <footer>
+          Copyright Josh Serbus 2019
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default App;
